d4272bf1a4352ab49fab1194cc303cf48ebf607c
Kareem Mohamed <kareem3d.a@gmail.com>
2014-07-01 23:01:49 +0200

    Adding access

--- app/config/database.php
+++ app/config/database.php
@@ -52,16 +52,16 @@ return array(
 			'prefix'   => '',
 		),
 
-		'mysql' => array(
-			'driver'    => 'mysql',
-			'host'      => 'localhost',
-            'database'  => 'akmo',
-            'username'  => 'root',
-            'password'  => '',
-			'charset'   => 'utf8',
-			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
-		),
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'speedupb_akmo',
+            'username'  => 'speedupb_kemo',
+            'password'  => '*&ADNSKYH$#A',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ),
 
 		'pgsql' => array(
 			'driver'   => 'pgsql',
